45b7bba74653fe47d1035cce054490cad455923d
WuCan <wucan@haocang.com>
2024-05-03 22:14:14 +0800

    feat: 删除频道

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -6,6 +6,7 @@ import { useParams, useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { ActionTooltip } from "@/components/action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
 interface ServerChannelProps {
     channel: Channel;
     server: Server;
@@ -24,6 +25,7 @@ export const ServerChannel = ({
     server,
     role,
 }:ServerChannelProps) => {
+    const { onOpen } = useModal();
     const router = useRouter();
     const params = useParams();
     
@@ -49,6 +51,7 @@ export const ServerChannel = ({
                         </ActionTooltip>
                         <ActionTooltip label="删除">
                             <Trash
+                            onClick={() => onOpen("deleteChannel",{server,channel})}
                             className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"/>
                         </ActionTooltip>
                     </div>
