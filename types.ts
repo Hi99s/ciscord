0c6146f26c38e770d494de9f962775797f903617
WuCan <wucan@haocang.com>
2024-05-05 16:29:05 +0800

    feat: 初始化socket.io

--- types.ts
+++ types.ts
@@ -1,6 +1,16 @@
-import { Profile, Server,Member } from "@prisma/client";
+import { Server as NetServer, Socket } from "net";
+import { NextApiResponse } from "next";
+import { Server as SocketIOServer } from "socket.io";
+import { Server, Member, Profile } from "@prisma/client"
 
-// 给频道加上成员和个人资料
 export type ServerWithMembersWithProfiles = Server & {
-    members: (Member & { profile: Profile })[];
-}
+  members: (Member & { profile: Profile })[];
+};
+
+export type NextApiResponseServerIo = NextApiResponse & {
+  socket: Socket & {
+    server: NetServer & {
+      io: SocketIOServer;
+    };
+  };
+};
