7abec34bddf4b67224c2067fccf93da40139914e
WuCan <wucan@haocang.com>
2024-05-13 05:29:21 +0800

    feat: 消息组件

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { cn } from "@/lib/utils";
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ModalProvider } from '@/components/providers/modal-provider'
 import { SocketProvider } from '@/components/providers/socket-provider';
+import { QueryProvider } from '@/components/providers/query-provider';
 const font = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -35,7 +36,9 @@ export default function RootLayout({
         {/* 提供模型 */}
         <SocketProvider>
         <ModalProvider />
+        <QueryProvider>
         {children}
+        </QueryProvider>
         </SocketProvider>
         </ThemeProvider>
     </body>
---
+++ components/chat/chat-messages.tsx
@@ -0,0 +1,99 @@
+"use client";
+import { Fragment } from "react";
+import { Member } from "@/types/member";
+import { Loader2, ServerCrash } from "lucide-react";
+
+import { useChatQuery } from "@/hooks/use-chat-query";
+
+import { ChatWelcome } from "./chat-welcome";
+
+
+type MessageWithMemberWithProfile = Message & {
+    member: Member & {
+      profile: Profile
+    }
+    }
+
+
+interface ChatMessagesProps {
+    name: string;
+    member: Member;
+    chatId: string;
+    apiUrl: string;
+    socketUrl: string;
+    socketQuery: Record<string, string>;
+    paramKey: "channelId" | "conversationId";
+    paramValue: string;
+    type: "channel" | "conversation";
+  }
+
+export const ChatMessages = ({
+    name,
+    member,
+    chatId,
+    apiUrl,
+    socketUrl,
+    socketQuery,
+    paramKey,
+    paramValue,
+    type,
+}: ChatMessagesProps) => {
+    const queryKey = `chat:${chatId}`;
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+        status,
+    } = useChatQuery({
+        queryKey,
+        apiUrl,
+        paramKey,
+        paramValue,
+    });
+    // 根据状态渲染不同的 UI
+    if (status === "loading") {
+        return (
+          <div className="flex flex-col flex-1 justify-center items-center">
+            <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+            <p className="text-xs text-zinc-500 dark:text-zinc-400">
+              消息加载中...
+            </p>
+          </div>
+        )
+      }
+    
+      if (status === "error") {
+        return (
+          <div className="flex flex-col flex-1 justify-center items-center">
+            <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+            <p className="text-xs text-zinc-500 dark:text-zinc-400">
+              出错了！
+            </p>
+          </div>
+        )
+      }
+    
+
+    return (
+        <div className="flex flex-col flex-1 py-4 overflow-y-auto">
+            <div className="flex-1"></div>
+            <ChatWelcome name={name} type={type} />
+            <div className="flex flex-col-reverse mt-auto">
+            <div className="flex flex-col-reverse mt-auto">
+        {data?.pages?.map((group, i) => (
+          <Fragment key={i}>
+            {
+                group.items.map((message: MessageWithMemberWithProfile) => (
+                    <div key={message.id}>
+                        { message.content}
+                    </div>
+                ))
+            }
+          </Fragment>
+        ))}
+      </div>
+            </div>
+        </div>
+    )
+};
